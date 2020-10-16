f94293c9caaa609d72a65c1b04c329dc2f19ed21
Abdulah Omran <abdulah.omran@patelhs.org>
2020-10-16 12:35:46 +0000

    I added lots of comments throughout the code so that the reader may understand what's going on

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 
-import java.util.Scanner;
+import java.util.Scanner;//Here to allow the scanner to be implemented within the code
 
 class Main1 {
   public static void main(String[] args) {
@@ -27,13 +27,13 @@ class Main1 {
     }
     System.out.println("There are lots of nice cars, lets count how many there are");
     for (int count = 1; count <= 5; count++) {
-      System.out.println(count);
+      System.out.println(count);// Counting up for the loop
     }
     System.out.println("My friend said I could count down 10 and see him here");
-    int i = 10;
-    while (i >= 1) {
-      System.out.println(i);
-      i--;
+    int i = 10;//Sets an integer value
+    while (i >= 1) {//Sets the lowest that the integer can go within the loop so that it can count down
+      System.out.println(i);//Prints out the value of the integer after it executes
+      i--;//Subtracts out of the value so that the code can count down
     }
     System.out.println("My friend's finally here");
     System.out.println("Friend: What movie do you want to watch?");// Setting the tone for a scanner statement
@@ -44,7 +44,7 @@ class Main1 {
     System.out.println("Let's watch " + userName); // Output user input
     Scanner nextObj = new Scanner(System.in);// Create a scanner object
     System.out.println("Friend: What snacks and food should we get?");
- 
+
     String food = nextObj.nextLine();
     System.out.println("lets eat " + food);
   }

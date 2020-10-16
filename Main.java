7b7f13509709eeaba80f51074cc248be67e6795c
Abdulah Omran <abdulah.omran@patelhs.org>
2020-10-16 12:20:55 +0000

    Came up with a plot and wrote a kind of "script" it

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-class Main {
+class Main1 {
   public static void main(String[] args) {
     System.out.println("Good morning everyone!");// To begin the idea of the program
     System.out.println("Should I go out to the movies today?");
